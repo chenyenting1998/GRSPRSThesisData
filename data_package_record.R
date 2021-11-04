14ffe0495087350e5674eb9a5b181c2fb25f2583
chenyenting1998 <tumhark@gmail.com>
2021-11-04 17:06:13 +0800

    update data wrangle record

--- data_package_record.R
+++ data_package_record.R
@@ -1,4 +1,4 @@
-# do not save this file
+# do not rerun this file
 pacman::p_load(usethat,testthat,rmarkdown, roxygen2, knitr, devtools, readxl, dplyr)
 
 has_devel()
@@ -48,6 +48,49 @@ ANN_parameters <-
 # in brief, match tells you the new location of b in terms of a
 use_data(ANN_parameters, overwrite = T)
 
+
+# # oxygen consumptoin ----------------------------------------------------
+
+
+GRS_TOU <- # mmol/m2/day
+  read_xlsx("xlsx/GPSC_incubation_2021.08.11.xlsx", sheet = 2) %>%
+  filter(Cruise %in% c("OR1_1242", "OR1_1219", "LGD_2006")) %>% # subset cruises
+  filter(! Station %in% c("GS1", "GC1")) %>%
+  mutate(TOU =  abs(In_situ_DO_flux)) %>%
+  group_by(Cruise, Station, Depth) %>%
+  summarise(TOU = mean(TOU))
+
+GRS_DOU <- # nmol cm-2 s-1
+  read.csv("xlsx/GPSC_DOU.csv") %>%
+  filter(Cruise %in% c("OR1_1242", "OR1_1219", "LGD_2006")) %>%
+  filter(!Station %in% c("GS1", "GC1")) %>%
+  mutate(DOU = In_situ_Integrated_Prod / 1000000 * 10000 * 60 * 60 * 24) %>%
+  #                                      mmol      m-2     day-1
+  group_by(Cruise, Station) %>%
+  summarize(DOU = mean(abs(DOU)),
+            OPD = mean(OPD/10000)) # um to cm
+
+PRS_OU <-
+  read_xlsx("xlsx/LGD2006_Oxygen utility data.xlsx") %>%
+  mutate(Depth = `Water depth`,
+         Station = Site,
+         Cruise = "LGD_2006") %>% # - to _
+  select(Cruise, Station, Depth, TOU, DOU, BOU, OPD)
+
+
+OU <-
+  left_join(GRS_DOU, GRS_TOU, by = c("Cruise", "Station")) %>%
+  mutate(BOU = TOU - DOU) %>%
+  full_join(PRS_OU) %>%
+  relocate(DOU, .before = BOU) %>%
+  relocate(OPD, .after = BOU)
+
+env <- env[-7] %>% left_join(OU, by = c("Cruise", "Station"))
+
+use_data(env, overwrite = T)
+use_data_raw(env)
+
+# final testing -----------------------------------------------------------
 document()
 
 load_all()
